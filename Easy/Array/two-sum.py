7017cef05514f3c16cb115bb6511be267987117e
Shavkatjon06 <shavkatjonjuraev219@gmail.com>
2024-10-11 10:08:01 +0500

    Palindrome Number - Easy/Math

--- Easy/Array/two-sum.py
+++ Easy/Array/two-sum.py
@@ -16,7 +16,7 @@ def twoSum(list, target):
     for i, x in enumerate(list):  # i am getting i = index, x = value
         diff = target - x  # let's say 4 = 9 - 5
         if diff in bag:  # if 4 is in my bag
-            return [bag[diff], i]  # i am returning index from my bag, and the other pair
+            return [bag[diff], i]  # i am returning index from my bag, and the other index
         bag[x] = i  # else I am assigning key:value to my bag
 print(twoSum([0,2,1,3,4], 5))
 print(twoSum([2,3,7,10,9], 11))
