aba20c45f58830fb8cda3f7a9b51101fefea1185
Shavkatjon06 <shavkatjonjuraev219@gmail.com>
2024-10-17 22:42:19 +0500

    remove target element - Easy/Arrays

---
+++ Easy/Array/remove-element.py
@@ -0,0 +1,10 @@
+def removeElement(nums, target):
+    left = 0  # This is where we put numbers that aren't the target
+    for i in range(len(nums)):  # Go through each item in the list
+        if nums[i] != target:  # If the number isn't the target we want to remove
+            nums[left] = nums[i]  # Put the number at the current 'left' spot
+            left += 1  # Move 'left' forward to the next spot
+    return left  # Return how many numbers are left after removing the target
+
+
+print(removeElement([3, 2, 2, 3], 3))
